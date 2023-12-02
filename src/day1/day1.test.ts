5f9dea5e93cee55a0505459c33bc21666b14f427
Kimmo Salmela <kimmo.salmela@reun.eu>
2023-12-02 16:00:54 +0200

    Day 1 puzzle 2 solution
    /spend 35m

--- src/day1/day1.test.ts
+++ src/day1/day1.test.ts
@@ -1,11 +1,42 @@
-import { expect, test } from "vitest";
-import { findCalibractionValue } from "./day1.js";
+import { expect, it, test, describe } from "vitest";
+import { findCalibractionValue, textOrNumberToNumber } from "./day1.js";
 
-test("find calibration value example", () => {
-  const input = `1abc2
-pqr3stu8vwx
-a1b2c3d4e5f
-treb7uchet`;
+describe("findCalibractionValue", () => {
+  it("should find the correct calibration value sum", () => {
+    const input = `two1nine
+  eightwothree
+  abcone2threexyz
+  xtwone3four
+  4nineeightseven2
+  zoneight234
+  7pqrstsixteen`;
 
-  expect(findCalibractionValue(input)).toBe(142);
+    expect(findCalibractionValue(input)).toBe(281);
+  });
+});
+
+describe("textOrNumberToNumber", () => {
+  it("should retain actual numbers as-is", () => {
+    expect(textOrNumberToNumber("3")).toBe(3);
+    expect(textOrNumberToNumber("5")).toBe(5);
+  });
+
+  it("should convert textual numbers to actual numbers", () => {
+    const numbers = [
+      "zero",
+      "one",
+      "two",
+      "three",
+      "four",
+      "five",
+      "six",
+      "seven",
+      "eight",
+      "nine",
+    ];
+    const fixtures = numbers.map((x, i) => ({ input: x, expected: i }));
+    fixtures.forEach((fixture) => {
+      expect(textOrNumberToNumber(fixture.input)).toBe(fixture.expected);
+    });
+  });
 });
--- src/day1/day1.ts
+++ src/day1/day1.ts
@@ -1,12 +1,39 @@
+const numbers = [
+  "zero",
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+];
+
+export function textOrNumberToNumber(text: string): number {
+  // Return digits as numbers.
+  if (!isNaN(Number(text))) {
+    return Number(text);
+  }
+
+  // Find out, which array index this textual number corresponds to and return
+  // it. There's no
+  return numbers.indexOf(text.toLowerCase());
+}
+
 export function findCalibractionValue(input: string): number {
+  // Construct number finding regex segment
+  const numberR = "\\d|" + numbers.join("|");
   // Regex to find first (and possibly last digit on each line)
-  const r = /^.*?(\d)(?:.*(\d)|.*$)/gm;
+  const r = new RegExp(`^.*?(${numberR})(?:.*(${numberR})|.*$)`, "gm");
   // Use `matchAll` to get all matches from each line
   const matches = [...input.matchAll(r)];
   // Combine digits. If a line only has a single digit, it is repeated twice.
-  const result = matches?.reduce(
-    (acc, m) => acc + Number(m[1] + (m[2] ?? m[1])),
-    0
-  );
+  const result = matches?.reduce((acc, m) => {
+    const number1 = textOrNumberToNumber(m[1]);
+    const number2 = textOrNumberToNumber(m[2] ?? m[1]);
+    return acc + Number(`${number1}${number2}`);
+  }, 0);
   return result;
 }
